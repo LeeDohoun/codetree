6cd27c439095cfcdd3137f2df46e0135ea989d4a
LEE DOHOUN <166192655+LeeDohoun@users.noreply.github.com>
2025-03-13 18:13:15 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 391ms, Memory: 76MB, Status: Runtime Error - CodeTree

---
+++ 250313/재귀함수를 이용한 별 출력/star-output-with-recursive-function.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        printstar(n);
+    }
+    public static void printstar(int n){
+        if(n==0)return;
+        printstar(n--);
+        
+        System.out.print(n);
+    }
+}
