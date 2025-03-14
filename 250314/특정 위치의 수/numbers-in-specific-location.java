6c7e5ff47236807e87464aa281a9f938f37f41bb
LEE DOHOUN <166192655+LeeDohoun@users.noreply.github.com>
2025-03-14 20:05:14 +0900

    Title: 특정 위치의 수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250314/특정 위치의 수/numbers-in-specific-location.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int[] arr=new int(10);
+        Scanner sc =new Scanner(Sytem.in);
+        for(int i=0;i<10;i++){
+            arr[i]=sc.nextInt();
+        }
+        int sum = arr[2]+arr[4]+arr[9];
+        Sytem.out.print(sum);
+    }
+}
