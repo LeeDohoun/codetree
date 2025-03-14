0f1c390a3f9924ac6292df1625e65fd68c6edd46
LEE DOHOUN <166192655+LeeDohoun@users.noreply.github.com>
2025-03-14 20:07:15 +0900

    Title: 특정 위치의 수, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 250314/특정 위치의 수/numbers-in-specific-location.java
+++ 250314/특정 위치의 수/numbers-in-specific-location.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        int[] arr=new int(10);
-        Scanner sc =new Scanner(Sytem.in);
+        int[] arr=new int[10];
+        Scanner sc =new Scanner(System.in);
         for(int i=0;i<10;i++){
             arr[i]=sc.nextInt();
         }
         int sum = arr[2]+arr[4]+arr[9];
-        Sytem.out.print(sum);
+        System.out.print(sum);
     }
 }
