0cc21ce8830062035565a9a1f14938e64f5dfbea
LEE DOHOUN <166192655+LeeDohoun@users.noreply.github.com>
2025-03-17 22:22:16 +0900

    Title: 함수를 이용한 369 게임, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 250317/함수를 이용한 369 게임/369-games-using-functions.java
+++ 250317/함수를 이용한 369 게임/369-games-using-functions.java
@@ -17,6 +17,10 @@ public class Main {
     }
 
     // 재귀함수: 숫자에 3, 6, 9가 포함되어 있는지 확인
+    /* 이런형식 말고 while(n!=0)안에서 % 통해서 해당 부분이 3,6,9인지 체크 하고 만약 그러면 true
+    아니면 n/=10; 아 while()문 빠져나오면 없는거니 false 반환.
+    
+    */
     public static boolean contains369(int n) {
         if (n == 0) return false;
         if (n % 10 == 3 || n % 10 == 6 || n % 10 == 9) return true;
